24e26a3edc9ffce5d388cfb53923e838a5a12936
Johannes Waldmann <joe>
2002-12-02 10:27:32 +0000

    Faktor

---
+++ util/Autolib/Util/Faktor.hs
@@ -0,0 +1,31 @@
+module Util.Faktor 
+
+( zerlege 
+, primfaktoren
+)
+
+
+where
+
+
+type Zerlegung = [(Integer, Int)]
+-- in Primzahlpotenzen
+
+zerlege :: Integer -> Zerlegung
+zerlege n | n <= 0 = error "Util.Zerlegung.zerlege"
+zerlege n = filter ( \ (p,e) -> e > 0 ) 
+	  $ helper 2 n 
+
+helper p 1 = []
+helper p n |  p*p > n  = [ (n,1) ]
+helper p n = 
+    let nrs = iterate ( \ (n, r) -> divMod n p ) ( n, 0 )
+	pre = takeWhile ( \ (n,r) -> r == 0 ) nrs 
+	(m, s) = last pre
+	d = if p == 2 then 1 
+	    else if 1 == p `mod` 6 then 4
+	    else 2
+    in ( p, length pre - 1 ) : helper (p+d) m
+
+primfaktoren :: Integer -> [ Integer ]
+primfaktoren = map fst . zerlege
