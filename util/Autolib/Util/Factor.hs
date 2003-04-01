63686fbbbafb8b0cf483a1efbb13ef98da4ac6b0
Johannes Waldmann <joe>
2003-04-01 19:32:12 +0000

    factor

---
+++ util/Autolib/Util/Factor.hs
@@ -0,0 +1,10 @@
+module Util.Factor where
+
+-- $Id$
+
+import List ( tails, isPrefixOf )
+
+factor :: Eq a => [a] -> [a] -> Bool
+factor x y = or $ do
+    z <- tails y
+    return $ isPrefixOf x z
