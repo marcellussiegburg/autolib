098693943217d74c8a624e6db209db0cf5e60e7c
Johannes Waldmann <joe>
2003-04-25 08:44:00 +0000

    kante

---
+++ graph/Autolib/Graph/Kante.hs
@@ -0,0 +1,40 @@
+module Graph.Kante where
+
+-- $Id$
+
+import Reader
+import ToDoc
+
+
+data Kante a  = Kante
+	      { von       :: a
+	      , nach      :: a
+	      } deriving (Eq, Ord)
+
+kante :: Ord a => a -> a -> (Kante a)
+-- ungerichteter Graph => Kanten von klein nach groß ordnen.
+kante x y = 
+    if x < y then Kante { von = x, nach = y }
+	     else Kante { von = y, nach = x }
+
+
+      
+instance ToDoc a => ToDoc (Kante a) where
+    toDocPrec p k = docParen (p >= fcp) 
+		  $ text "kante" <+> 
+		    sep ( map ( toDocPrec fcp )  [ von k, nach k ] )
+
+instance (ToDoc a) => Show (Kante a) where
+  show = render . toDoc
+
+
+instance (Ord a, Reader a) => Reader (Kante a) where
+    readerPrec d = readerParen (d > 9) 
+      $ do my_reserved "kante"
+	   v <- readerPrec 10
+	   n <- readerPrec 10
+	   return $ kante v n
+    
+instance  (Ord a, Reader a ) => Read ( Kante a ) where
+     readsPrec = parsec_readsPrec
+
