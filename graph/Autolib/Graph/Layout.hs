c35b61124fb2a9b0db8703b31765ca5689615d5c
Johannes Waldmann <joe>
2003-04-25 08:44:31 +0000

    layout

---
+++ graph/Autolib/Graph/Layout.hs
@@ -0,0 +1,20 @@
+module Graph.Layout where
+
+-- $Id$
+
+import Graph.Graph
+
+unlabelled :: Graph a -> Graph a
+unlabelled g = g { show_labels = False
+		   , layout_hints = "-Nwidth=0.1" : layout_hints g
+		   }
+
+neato :: Graph a -> Graph a
+neato g = g { layout_program = "neato" }
+
+dot :: Graph a -> Graph a
+dot g = g { layout_program = "dot" }
+
+twopi :: Graph a -> Graph a
+twopi g = g { layout_program = "twopi" }
+
