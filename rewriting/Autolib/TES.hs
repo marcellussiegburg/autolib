5b754a26a789bd3bf64e31e6328db9a326f4e710
Johannes Waldmann <joe>
2003-06-04 15:19:17 +0000

    generic

---
+++ rewriting/Autolib/TES.hs
@@ -0,0 +1,16 @@
+module TES 
+
+-- $Id$
+
+( module TES.Symbol
+, module TES.Term
+, module TES.Rule
+, module TES.Data
+)
+
+where
+
+import TES.Symbol
+import TES.Term
+import TES.Rule
+import TES.Data
