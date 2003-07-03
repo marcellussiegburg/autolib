9e227be91ca5e0ec6315792291e56bdb43d9eb48
Johannes Waldmann <joe>
2003-07-03 21:23:56 +0000

    file

---
+++ relation/Autolib/Relation/Basic.hs
@@ -0,0 +1,19 @@
+module Relation.Basic where
+
+-- $Id$
+
+import Relation.Type
+import Relation.Ops
+
+
+empty :: Ord a => Set a -> Type a a
+empty s = Make { source = s, target = s, unRelation = emptyFM }
+
+flat :: Ord a => Set a -> Type a a
+flat s = reflex $ empty s
+
+linear :: Ord a => [a] -> Type a a
+linear xs =
+    let s = mkSet xs
+    in  reflex_trans $ ( make $ zip xs $ tail xs ) { source = s, target = s }
+
