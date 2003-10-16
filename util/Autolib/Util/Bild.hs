6e4f3edeb7b42017faccb35b468a625b70894815
Johannes Waldmann <joe>
2003-10-16 07:37:42 +0000

    neu

---
+++ util/Autolib/Util/Bild.hs
@@ -0,0 +1,31 @@
+module Util.Bild 
+
+( Bild 
+, ToBild (..)
+, module Data.Array
+)
+
+where
+
+-- $Id$
+
+import Data.Array
+
+import ToDoc
+
+type Bild = Array (Int, Int) Char
+
+instance ToDoc Bild where
+    toDoc b = vcat $ do
+	let ((l,u),(r,o)) = bounds b
+	y <- reverse [ u .. o ]
+	return $ text $ do
+	    x <- [ l ..  r ]
+	    return $ b ! (x,y)
+
+instance Show Bild where show = render . toDoc
+
+class ToBild a where
+      toBild :: a -> Bild
+
+
