bc44b778490b1625e36e177de820cbc7248c4d89
Johannes Waldmann <joe>
2003-10-28 09:27:04 +0000

    make trim, simplify code

--- fa/Autolib/NFA/Factors.hs
+++ fa/Autolib/NFA/Factors.hs
@@ -3,24 +3,18 @@ module NFA.Factors where
 -- $Id$
 
 import NFA.Type
-import Set
-import Monad ( guard )
-import FiniteMap 
+import NFA.Trim
 
-import ToDoc ( ToDoc, toDoc )
+import Set
 
 -- bestimme die Sprache aller (zusammenhängenden) Teilwörter
 
-factors :: NFAC c Int
-	=> NFA c Int -> NFA c Int
-factors a =
-    let s = succ $ maximum $ 0 : lstates a -- neuer zustand
-        f = NFA { nfa_info = funni "factors" [ info a ]
-		, states = states a `union` unitSet s
-		, starts = states f -- alles
-		, finals = states f -- auch alles
-		, trans  = plusFM_C union ( trans a ) $ collect $ do
-	             ( p, c, q ) <- unCollect $ trans a
-	             return ( s, c, q )
-		}
-    in  f
+factors :: NFAC c a
+	=> NFA c a -> NFA c a
+factors a0 =
+    let a = trim a0 
+        -- ohne trim stimmts nicht, siehe mail von Mirko (28. 10.)
+    in  a   { nfa_info = funni "factors" [ info a ]
+ 	    , starts = states a -- alles
+	    , finals = states a -- auch alles
+	    }
