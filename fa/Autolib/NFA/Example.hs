b41d31f7e9628a42f12d560cbd0e14aecb2c652f
Johannes Waldmann <joe>
2003-11-12 14:48:01 +0000

    benutze customized alphabet in beispiel-automaten

--- fa/Autolib/NFA/Example.hs
+++ fa/Autolib/NFA/Example.hs
@@ -4,16 +4,22 @@ module NFA.Example where
 
 import NFA
 import ToDoc
+import Sets
 
 example :: NFA Char Int
-example = NFA 
+example = example_sigma $ mkSet "ab"
+
+example_sigma :: Set Char -> NFA Char Int
+example_sigma s =
+    let a : b : _ = setToList s
+    in  NFA 
     { nfa_info = text "example"
     , states = mkSet [ 1, 2, 3]
     , starts = mkSet [ 2]
     , finals = mkSet [ 2]
-    , trans = listToFM [ ((1, 'a'), mkSet [ 2])
-                       , ((2, 'a'), mkSet [ 1])
-                       , ((2, 'b'), mkSet [ 3])
-                       , ((3, 'b'), mkSet [ 2])
+    , trans = listToFM [ ((1, a), mkSet [ 2])
+                       , ((2, a), mkSet [ 1, 3])
+                       , ((2, b), mkSet [ 3])
+                       , ((3, b), mkSet [ 2])
                        ]
     }
