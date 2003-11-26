ca4be8479c6aae524b729ed5bdd9fc2c7c44a6f0
Johannes Waldmann <joe>
2003-11-26 09:22:35 +0000

    reduce parameters (slightly)

--- reporter/Autolib/Timer.hs
+++ reporter/Autolib/Timer.hs
@@ -13,19 +13,23 @@ import Concurrent
 timed :: Int -> a -> IO a -> IO a
 timed d def action = do
     ch <- newChan
+
     apid <- forkIO $ do
          x <- action
 	 writeChan ch x
+
     tpid <- forkIO $ do
-         sequence_ $ do
+         sleep d
+	 writeChan ch def
+
+    x <- readChan ch
+    -- killThread apid    
+    -- killThread tpid
+    return x
+
+sleep d = sequence_ $ do
              i <- [ 1 .. d ]
 	     return $ do threadDelay $ 10^6
 			 yield
-	 writeChan ch def
-    x <- readChan ch
-    killThread apid
-    killThread tpid
-    return x
-
 
 
