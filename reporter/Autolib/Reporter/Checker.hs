2f25c9bb855caa0506f5bda2971848b8f1555911
Johannes Waldmann <joe>
2003-12-10 09:50:49 +0000

    kellerautomaten

--- reporter/Autolib/Reporter/Checker.hs
+++ reporter/Autolib/Reporter/Checker.hs
@@ -22,8 +22,19 @@ make tag doc inv = Make { nametag = tag, condition = doc, investigate = inv }
 run :: Type a -> a -> Reporter ()
 run c g = do
     inform $ condition c
-    investigate c g
+    nested 4 $ investigate c g
 
+and_then :: Type a -> Type a
+     -> Type a
+-- beides ausführen, 
+-- aber kurznamen nur vom rechten anzeigen
+and_then l r = Make
+	 { nametag = nametag r
+	 , condition = condition r
+	 , investigate = \ a -> do
+	       Reporter.Checker.run l a
+	       Reporter.Checker.run r a
+         }
 
 
 
