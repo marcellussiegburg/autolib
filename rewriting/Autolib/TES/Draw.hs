5ec49672272dd87a8adb08ea54ee97d08cc40d8f
Johannes Waldmann <joe>
2004-04-14 10:00:55 +0000

    file

---
+++ rewriting/Autolib/TES/Draw.hs
@@ -0,0 +1,33 @@
+module TES.Draw 
+
+( draw )
+
+where
+
+--  $Id$
+
+import TES.Type
+import qualified Data.Tree as D
+import ToDoc
+
+data Container = Container String
+instance Show Container where
+    show ( Container c ) = c
+
+make :: ( Show v, Show c )
+     => Term v c 
+     -> D.Tree Container
+make = tfold ( \ v -> D.Node ( Container $ show v ) [] )
+             ( \ c xs -> D.Node ( Container $ show c ) $ reverse xs )
+
+draw :: ( Show v, Show c )
+     => Term v c
+     -> Doc
+draw = vcat
+     . map text
+     . lines
+     . D.drawTree
+     . make
+
+
+
