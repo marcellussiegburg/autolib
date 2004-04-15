90f096c49a45c468a519df3cb58feae56a2e94a2
Johannes Waldmann <joe>
2004-04-15 23:38:04 +0000

    add NFA.Link take care of module names with dots

---
+++ fa/Autolib/NFA/Link.hs
@@ -0,0 +1,33 @@
+-- |  add transitions to automaton
+-- possibly containing new states
+
+module NFA.Link where
+
+--  $Id$
+
+import NFA.Type
+import ToDoc
+
+link :: NFAC c a 
+     => NFA c a 
+     -> (a, c, a) 
+     -> NFA c a
+link a l @ (p, c, q) 
+     = informed ( funni "link" [ info a, toDoc l ] )
+     $ links a [ l ]	     
+
+links :: NFAC c a 
+     => NFA c a 
+     -> [ (a, c, a) ]
+     -> NFA c a
+links a ls =
+     a { nfa_info = funni "links" [ info a, toDoc ls ]
+       , states = union ( states a ) $ mkSet $ do 
+	    (p, c, q) <- ls
+	    [p, q]
+       , trans  = addListToFM_C union ( trans a ) $ do
+	    (p, c, q) <- ls
+	    return ( (p, c), unitSet q )
+       }
+
+
