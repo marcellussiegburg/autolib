7d3026ea2f40e6831a8953594e7925ed5a06dd33
Johannes Waldmann <joe>
2004-05-05 23:27:33 +0000

    file

---
+++ fta/Autolib/NFTA/Normalize.hs
@@ -0,0 +1,13 @@
+module NFTA.Normalize where
+
+--  $Id$
+
+import NFTA
+import Data.FiniteMap
+
+normalize :: ( NFTAC c s, NFTAC c Int )
+	  => NFTA c s -> NFTA c Int
+normalize a = 
+    let fm = listToFM $ zip (lstates a) [0 ..]
+	fun = lookupWithDefaultFM fm ( error "NFTA.normalize" )
+    in  statemap fun a
