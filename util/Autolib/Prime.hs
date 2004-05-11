d27987a2aa8a802c2c15006e536c040c24c2c65a
Johannes Waldmann <joe>
2004-05-11 16:37:07 +0000

    specify type

--- util/Autolib/Prime.hs
+++ util/Autolib/Prime.hs
@@ -5,5 +5,5 @@ module Prime where
 prime :: Integer -> Bool
 prime n | n < 2 = False
 prime n = all ( \ t -> 0 /= mod n t ) 
-	      [ 2 .. truncate $ sqrt $ fromIntegral n ]
+	      [ 2 .. truncate $ sqrt ( fromIntegral n :: Double ) ]
 
