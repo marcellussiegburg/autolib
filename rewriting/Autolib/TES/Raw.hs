94a012a0235931ab6b1828eb9dfbb7c98d7023a6
Johannes Waldmann <joe>
2004-08-20 13:31:45 +0000

    file

---
+++ rewriting/Autolib/TES/Raw.hs
@@ -0,0 +1,14 @@
+-- | fixme: wrong name (TES/Term confusion)
+
+module TES.Raw where
+
+--  $Id$
+
+import Data.Typeable
+
+-- | TODO: include 'hash' data field in record (cached)
+data Term v c = Node !c ![ Term v c ]
+	      | Var  !v
+     deriving ( Eq, Ord, Typeable )
+
+
