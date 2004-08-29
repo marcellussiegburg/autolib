e762e5ecbbed3c10852d53a48b372aa1401a3b16
Johannes Waldmann <joe>
2004-08-29 19:08:11 +0000

    add

---
+++ rewriting/Autolib/Rewriting.hs
@@ -0,0 +1,10 @@
+module Autolib.Rewriting where
+
+--  $Id$
+
+import Autolib.Rewriting.Address
+import Autolib.Rewriting.Automaton
+import Autolib.Rewriting.Path
+import Autolib.Rewriting.Strings
+import Autolib.Rewriting.Terms
+
---
+++ util/Autolib/Util.hs
@@ -0,0 +1,30 @@
+module Autolib.Util where
+
+--  $Id$
+
+import Autolib.Util.BFS
+import Autolib.Util.Bild
+import Autolib.Util.Cache
+import Autolib.Util.Chmod
+import Autolib.Util.Closure
+import Autolib.Util.DPL
+import Autolib.Util.DPL_Set
+import Autolib.Util.Debug
+import Autolib.Util.Doppelte
+import Autolib.Util.Doppler
+import Autolib.Util.Edit
+import Autolib.Util.Factor
+import Autolib.Util.Faktor
+import Autolib.Util.Fix
+import Autolib.Util.Generate
+import Autolib.Util.Hide
+import Autolib.Util.Mehrfache
+import Autolib.Util.Perm
+import Autolib.Util.Seed
+import Autolib.Util.Size
+import Autolib.Util.Sort
+import Autolib.Util.Splits
+import Autolib.Util.Teilfolgen
+import Autolib.Util.Uniq
+import Autolib.Util.Wort
+import Autolib.Util.Zufall
