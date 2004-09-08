f370ebedf253947738d52145c65cb9c48026371f
Johannes Waldmann <joe>
2004-09-08 08:40:34 +0000

    file

---
+++ graph/Autolib/Graph.hs
@@ -0,0 +1,37 @@
+--  $Id$
+
+module Autolib.Graph where
+
+import Autolib.Graph.Basic
+import Autolib.Graph.Beispiele
+import Autolib.Graph.Brinkmann
+-- import Autolib.Graph.CLQ12-Solve
+-- import Autolib.Graph.Clique
+-- import Autolib.Graph.Clique_Solve
+-- import Autolib.Graph.Color
+import Autolib.Graph.Display
+import Autolib.Graph.Gallery
+import Autolib.Graph.Generate
+import Autolib.Graph.Graph
+-- import Autolib.Graph.HAM12-Solve
+-- import Autolib.Graph.Hamilton
+-- import Autolib.Graph.Hamilton_Solve
+import Autolib.Graph.Incidence
+import Autolib.Graph.Isomorph
+-- import Autolib.Graph.Joe
+import Autolib.Graph.Kante
+import Autolib.Graph.Kneser
+import Autolib.Graph.Knight
+import Autolib.Graph.Labeling
+import Autolib.Graph.Layout
+import Autolib.Graph.Line
+
+import Autolib.Graph.Mycielski
+import Autolib.Graph.Ops
+-- import Autolib.Graph.Quiz
+-- import Autolib.Graph.Standard
+-- import Autolib.Graph.Type
+-- import Autolib.Graph.Util
+import Autolib.Graph.Valid
+-- import Autolib.Graph.Vis
+-- import Autolib.Graph.Viz
