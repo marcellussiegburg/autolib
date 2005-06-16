2f5c2264f98cbfe498b705b93446e97a034c7f1d
Johannes Waldmann <joe>
2005-06-16 20:58:39 +0000

    file

---
+++ fa/Autolib/ENFA/Uneps.hs
@@ -0,0 +1,20 @@
+module Autolib.ENFA.Uneps where
+
+--  $Id$
+
+import Autolib.ENFA.Data
+
+import qualified Autolib.NFA as N
+import Autolib.NFA.Epsilon
+import qualified Autolib.Relation as R
+
+uneps :: NFAC c s => ENFA c s -> N.NFA c s
+uneps a = 
+          N.NFA { N.nfa_info = enfa_info a
+		, N.alphabet = alphabet a
+		, N.states  = states a
+		, N.starts = starts a
+		, N.finals = finals a
+		, N.trans = trans a
+		}
+   `add_epsilons` R.pairs ( eps a )
