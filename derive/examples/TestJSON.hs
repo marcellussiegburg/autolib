9b5aa7716017244b82eebfe6dfc7f58bfeecb44d
Bertram Felgenhauer <int-e@gmx.de>
2009-08-26 18:58:14 +0200

    derive: Add derivation for Text.JSON

---
+++ derive/examples/TestJSON.hs
@@ -0,0 +1,26 @@
+{-# OPTIONS_GHC -ddump-splices #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module TestJSON where
+
+import Data.Derive.JSON
+import Text.JSON
+
+data MMaybe a = JJust a | NNothing
+    deriving (Eq, Show)
+
+data Frotz = A | B Frotz | C { sub :: Frotz } | D { sub :: Frotz, foo :: Frotz }
+    deriving (Eq, Show)
+
+data Hard = Aaa | AAa | AAA { aaa :: Hard }
+    deriving (Eq, Show)
+
+data X = () :+: ()
+
+$(derives [makeJSON] [''MMaybe, ''Frotz, ''Hard, ''X])
+
+t1 :: MMaybe Frotz
+t1 = JJust (D { sub = B A, foo = C { sub = A } })
+
+t2 :: [Hard]
+t2 = [Aaa, AAa, AAA AAa]
