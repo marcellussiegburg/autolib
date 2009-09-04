4278f7114c66af4e3700546e435be187c5730519
Bertram Felgenhauer <int-e@gmx.de>
2009-09-04 09:41:16 +0200

    cabalize

---
+++ transport/Data/Autolib/Transport.hs
@@ -0,0 +1,9 @@
+module Data.Autolib.Transport (
+    module Data.Autolib.Transport.Class,
+    module Data.Autolib.Transport.Atom
+) where
+
+import Data.Autolib.Transport.Class
+import Data.Autolib.Transport.Atom
+import Data.Autolib.Transport.JSON ()
+import Data.Autolib.Transport.HaXR ()
