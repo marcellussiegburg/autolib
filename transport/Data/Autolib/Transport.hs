44f5acec3d745d0b987e195ddd56083fb0cd8b8e
Bertram Felgenhauer <int-e@gmx.de>
2009-10-07 16:53:34 +0200

    add ToTransport instances for Set and Map

--- transport/Data/Autolib/Transport.hs
+++ transport/Data/Autolib/Transport.hs
@@ -4,7 +4,8 @@
 module Data.Autolib.Transport (
     module Data.Autolib.Transport.Class,
     module Data.Autolib.Transport.Atom,
-    module Data.Autolib.Transport.Error
+    module Data.Autolib.Transport.Error,
+    module Data.Derive.ToTransport
 ) where
 
 import Data.Autolib.Transport.Class
@@ -12,17 +13,38 @@ import Data.Autolib.Transport.Atom
 import Data.Autolib.Transport.Error
 import Data.Autolib.Transport.JSON ()
 import Data.Autolib.Transport.HaXR ()
+import Data.Derive.ToTransport (makeToTransport, derives)
 
 import Data.ByteString (ByteString)
+import qualified Data.Set as S
+import qualified Data.Map as M
 
 instance ToTransport () where
     toTransport   () = TrArray []
     fromTransport (TrArray []) = return ()
     fromTransport _ = fail "expected empty TrArray"
 
+instance (ToTransport a, ToTransport b) => ToTransport (a, b) where
+    toTransport (a, b) = TrArray [toTransport a, toTransport b]
+    fromTransport (TrArray [a, b]) = do
+        a' <- fromTransport a
+        b' <- fromTransport b
+        return (a', b')
+    fromTransport _ = fail "expected two-element TrArray"
+
 instance ToTransport a => ToTransport [a] where
     toTransport = TrArray . map toTransport
     fromTransport (TrArray xs) = mapM fromTransport xs
+    fromTransport _ = fail "expected TrArray"
+
+instance (Ord a, ToTransport a) => ToTransport (S.Set a) where
+    toTransport = TrArray . map toTransport . S.toList
+    fromTransport (TrArray xs) = S.fromList `fmap` mapM fromTransport xs
+    fromTransport _ = fail "expected TrArray"
+
+instance (Ord a, ToTransport a, ToTransport b) => ToTransport (M.Map a b) where
+    toTransport = TrArray . map toTransport . M.toList
+    fromTransport (TrArray xs) = M.fromList `fmap` mapM fromTransport xs
     fromTransport _ = fail "expected TrArray"
 
 -- instances for atoms
