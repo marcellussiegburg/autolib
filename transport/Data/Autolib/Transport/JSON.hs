a28b713bdb3711cc7d7ebf9acf8f0bd65bed0684
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2011-06-24 22:34:32 +0200

    ghc-7.2 wants this

--- transport/Data/Autolib/Transport/JSON.hs
+++ transport/Data/Autolib/Transport/JSON.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances #-}
+{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances, FlexibleInstances #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 -- |
