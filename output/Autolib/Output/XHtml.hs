01d3002882afb7c67b0653a437b7cbef66197728
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2013-04-16 20:41:11 +0200

    reformat (no change)

--- output/Autolib/Output/XHtml.hs
+++ output/Autolib/Output/XHtml.hs
@@ -7,8 +7,10 @@ import qualified Autolib.Multilingual.Html as Html
 
 instance Render Html.Html where
     render (Text t) = Html.stringToHtml t
-    render (Doc d)  = Html.pre $ Html.primHtml $ fmap show  d 
-    render (Pre d)  = Html.pre $ Html.primHtml $ fmap show  d 
+    render (Doc d)  = 
+        Html.pre $ Html.primHtml $ fmap show  d 
+    render (Pre d)  = 
+        Html.pre $ Html.primHtml $ fmap show  d 
     render (Image src _) = 
 	Html.image Html.! [ Html.src src, Html.alt src ]
     render (Link ref) =  
@@ -24,6 +26,7 @@ instance Render Html.Html where
 
     render (Above x Empty) = render x
     render (Above Empty y) = render y
+
     render (Above x y    ) =
         let rx = render x :: Html.Html
 	    ry = render y :: Html.Html
@@ -38,7 +41,8 @@ instance Render Html.Html where
 	    ry = render y :: Html.Html
         in  rx Html.+++ ry
 
-    render (Itemize xs) = Html.itemize $ map ( Html.li . render ) xs
+    render (Itemize xs) = 
+        Html.itemize $ map ( Html.li . render ) xs
     render (Nest x) = Html.blockquote $ render x
     render (Figure a b) = render (Above a b)
 
