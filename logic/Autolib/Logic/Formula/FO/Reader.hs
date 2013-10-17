ab48dd451fb7a68a0b6af93f1a727e025e9f9fe0
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2013-10-17 12:15:59 +0200

    separate parser (no PHOAS)

--- logic/Autolib/Logic/Formula/FO/Reader.hs
+++ logic/Autolib/Logic/Formula/FO/Reader.hs
@@ -1,59 +1,90 @@
+-- see discussion at
+-- http://stackoverflow.com/questions/19415621/parsing-for-phoas-expressions
+
 module Autolib.Logic.Formula.FO.Reader where
 
 import Autolib.Logic.Formula.FO.Data
+import Autolib.Logic.Formula.FO.ToDoc () -- instance
 import Autolib.Logic.Formula.Name
 
 import Autolib.Reader
 import Text.Parsec.Expr
+import Text.Parsec.Prim ( sysUnExpectError )
 
-expr = do
-    f <- exprF undefined
-    return $ f undefined
+import qualified Data.Set as S
+import qualified Data.Map as M
 
-exprF :: ( String -> n )
-      -> Parser ( Form n )
-exprF env = buildExpressionParser
-    [ [ prefix "not" Not  ]
-    , [ binary "&&" And AssocLeft ]
-    , [ binary "||" Or AssocLeft ]
-    , [ binary "=>" Implies AssocRight ]
-    ] ( atomF env )
+formula = do
+    f <- expr S.empty
+    return $ Formula $ convert undefined f 
 
-atomF env = my_parens (exprF  env)
-    <|> quantifiedF env
-    <|> basicF env
+-- this is what I was hoping to avoid:
+-- going via plain abstract syntax
+convert :: (Name -> n) -> E -> Form n
+convert env e = case e of
+    ELess x y -> Less (env x) (env y)
+    ESucc x y -> Succ (env x) (env y)
+    ELetter x y -> Letter x (env y)
+    ENot f -> Not ( convert env f )
+    EAnd f g -> And (convert env f)(convert env g)
+    EOr f g -> Or (convert env f)(convert env g)
+    EImplies f g -> Implies (convert env f)(convert env g)
+    EForall n f -> Forall $ \ a -> 
+        convert (extend env n a) f
+    EExists n f -> Exists $ \ a -> 
+        convert (extend env n a) f
 
+extend env name val = \ v -> 
+    if v == name then val else env v
 
-quantifiedF env = 
-       ( my_reserved "forall" >> contF Forall env )
-   <|> ( my_reserved "exists" >> contF Exists env )
+data E = ELess Name Name
+       | ESucc Name Name
+       | ELetter Name Name
+       | ENot E | EAnd E E | EOr E E | EImplies E E
+       | EForall Name E | EExists Name E
+    deriving ( Show )
 
-contF q env = do
-    n <- my_identifier ; my_symbol ":"
-    let env' = x( my_reserved n >> return Env ) <|> env
-    body <- exprF env'
-    return $ \ val0 -> q $ \ val1 -> body val1 -- WHAT
+expr :: S.Set Name -> Parser E
+expr bound = buildExpressionParser
+    [ [ prefix "not" ENot  ]
+    , [ binary "&&" EAnd AssocLeft ]
+    , [ binary "||" EOr AssocLeft ]
+    , [ binary "=>" EImplies AssocRight ]
+    ] ( atom  bound )
 
-basicF :: Parser (n -> Form n)
-       -> Parser (n -> Form n)
-basicF env = do
-    x <- env ; my_symbol "<"; y <- env
-    return $ \ val -> 
-        Less (unEnv $ x val) (unEnv $ y val)
+atom bound = my_parens (expr bound)
+    <|> quantified bound
+    <|> basic bound
 
-unEnv (Env v) = v
+quantified bound = 
+       ( my_reserved "forall" >> cont EForall bound )
+   <|> ( my_reserved "exists" >> cont EExists bound )
 
-{-
-basicF var = name >>= \ x -> case resolve env x of
-        Nothing -> do
-            y <- reference env
-            return $ Letter x y
-        Just x -> 
-             do my_reservedOp "<1" ; y <- reference env
-                return $ Succ x y
-         <|> do my_reservedOp "<" ; y <- reference env
-                return $ Less x y
--}
+cont q bound = do
+    var <- name ; my_symbol ":"
+    body <- expr $ S.insert var bound
+    return $ q var body
+
+basic bound = name >>= \ x -> 
+    if S.member x bound
+    then do
+        s <-  try ( my_symbol "<1" >> return ESucc )
+          <|> ( my_symbol "<"  >> return ELess )
+        y <- reference bound
+        return $ s x y
+    else do 
+        y <- my_parens (reference bound)
+        return $ ELetter x y
+
+name = fmap Name my_identifier
+
+-- TODO: how to set the error position?
+reference :: S.Set Name -> Parser Name
+reference bound = do
+    n <- name
+    if S.member n bound
+        then return n
+        else fail $ "name not bound: " ++ show n
 
 prefix  name fun       = 
     Prefix (do{ my_reserved name; return fun })
