176d2edb0d5a1acfb88912e4a0f51120f6e7228b
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2013-10-17 10:23:38 +0200

    state (not working)

--- logic/Autolib/Logic/Formula/FO/Reader.hs
+++ logic/Autolib/Logic/Formula/FO/Reader.hs
@@ -6,52 +6,42 @@ import Autolib.Logic.Formula.Name
 import Autolib.Reader
 import Text.Parsec.Expr
 
--- WHY NOT? expr :: Parser (forall n . Form n) 
 expr = do
-    f <- exprF ( fail "global variable" )
+    f <- exprF undefined
     return $ f undefined
 
-exprF :: Parser (n -> Form n) 
-      -> Parser (n -> Form n)
-exprF var = buildExpressionParser
-    [ [ prefix "not" notF  ]
-    , [ binary "&&" andF AssocLeft ]
-    , [ binary "||" orF AssocLeft ]
-    , [ binary "=>" impliesF AssocRight ]
-    ] ( atomF var )
+exprF :: ( String -> n )
+      -> Parser ( Form n )
+exprF env = buildExpressionParser
+    [ [ prefix "not" Not  ]
+    , [ binary "&&" And AssocLeft ]
+    , [ binary "||" Or AssocLeft ]
+    , [ binary "=>" Implies AssocRight ]
+    ] ( atomF env )
 
-notF f = \ val -> Not (f val)
-andF f g = \ val -> And (f val) (g val)
-orF f g = \ val -> Or (f val) (g val)
-impliesF f g = \ val -> Implies (f val) (g val)
-
-atomF var = my_parens (exprF  var)
-    <|> quantifiedF var
-    <|> basicF var
+atomF env = my_parens (exprF  env)
+    <|> quantifiedF env
+    <|> basicF env
 
 
-quantifiedF var = 
-       ( my_reserved "forall" >> contF Forall var )
-   <|> ( my_reserved "exists" >> contF Exists var )
+quantifiedF env = 
+       ( my_reserved "forall" >> contF Forall env )
+   <|> ( my_reserved "exists" >> contF Exists env )
 
-
-contF :: ( (n -> Form n) -> Form n )
-      -> Parser (n -> Form n)
-      -> Parser (n -> Form n)
-contF q var = do
+contF q env = do
     n <- my_identifier ; my_symbol ":"
-    let var' = ( my_reserved n >> return Var ) <|> var
-    body <- exprF var'
+    let env' = x( my_reserved n >> return Env ) <|> env
+    body <- exprF env'
     return $ \ val0 -> q $ \ val1 -> body val1 -- WHAT
 
 basicF :: Parser (n -> Form n)
        -> Parser (n -> Form n)
-basicF var = do
-    x <- var ; my_symbol "<"; y <- var
+basicF env = do
+    x <- env ; my_symbol "<"; y <- env
     return $ \ val -> 
-        Less (unVar $ x val) (unVar $ y val)
+        Less (unEnv $ x val) (unEnv $ y val)
 
-unVar (Var v) = v
+unEnv (Env v) = v
 
 {-
 basicF var = name >>= \ x -> case resolve env x of
